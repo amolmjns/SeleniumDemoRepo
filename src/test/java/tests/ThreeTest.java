e3c710d063aab5593097d9e10af081c707331617
amolmjns <159312375+amolmjns@users.noreply.github.com>
2024-02-08 01:30:58 +0530

    Updated by Jagruti

--- src/test/java/tests/ThreeTest.java
+++ src/test/java/tests/ThreeTest.java
@@ -12,6 +12,8 @@ public class ThreeTest extends Base1 {
 	@Test
 	public void threeTest() throws IOException, InterruptedException {
 
+		System.out.println("this is new code which is written by Jagruti Mahajan");
+
 		driver = InitialiseBrowser();
 		driver.get("https://www.amazon.in/");
 		Thread.sleep(3000);
